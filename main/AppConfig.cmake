c15989ae4cfab5546a75c0fdb1c9a48ff3b54eaf
Kevin <zhaoyl0704@thundersoft.com>
2023-01-18 14:03:13 +0800

    Add function of compiling aws configuration

---
+++ main/AppConfig.cmake
@@ -0,0 +1,19 @@
+message("Config! Here is app config aws client info .cmake\n")
+
+
+
+
+set(CMake_AWS_WEBRTC_CHANNEL                   "\"ts_anyke\"")
+set(CMake_AWS_DEFAULT_REGION                   "\"us-west-2\"")
+set(CMake_AWS_KVS_CACERT_PATH                  "\"/mnt/cert.pem\"")
+set(CMake_AWS_ACCESS_KEY_ID                    "\"AKIA4MK3YHF5JLUZ2VZB\"")
+set(CMake_AWS_SECRET_ACCESS_KEY                "\"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\"")
+set(CMake_AWS_SESSION_TOKEN                    "\"on6\"")
+set(CMake_AWS_IOT_CORE_THING_NAME              "\"ts_anyke\"")
+set(CMake_AWS_IOT_CORE_CREDENTIAL_ENDPOINT     "\"c1n2srkjlj4jwd.credentials.iot.us-west-2.amazonaws.com\"")
+set(CMake_AWS_IOT_CORE_CERT                    "\"/mnt/certifcate.pem.crt\"")
+set(CMake_AWS_IOT_CORE_PRIVATE_KEY             "\"/mnt/private.pem.key\"")
+set(CMake_AWS_IOT_CORE_ROLE_ALIAS              "\"AmKVSWebRTCIoTCredentialIAMRoleAlias\"")
+set(CMake_AWS_KVS_LOG_LEVEL                    "\"2\"")
+set(CMake_AWS_ENABLE_FILE_LOGGING              "\"OFF\"")
+set(CMake_AWS_DEBUG_LOG_SDP                    "\"OFF\"")
